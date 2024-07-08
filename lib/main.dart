17d441f8f4f8b423373afb21a3f2d34a70b51713
bismafaza <bismafaza@gmail.com>
2024-07-08 09:17:08 +0000

    news

--- lib/main.dart
+++ lib/main.dart
@@ -1,40 +1,46 @@
-import "package:flutter/material.dart";
-import "package:myapp/container_widget.dart";
+import 'package:flutter/material.dart';
+import 'package:myapp/container_widget.dart';
+import 'package:myapp/news_container.dart';
 
-void main () {
-  runApp(
-    MyApp()
-  );
+void main() {
+  runApp(MyApp());
 }
+
 class MyApp extends StatelessWidget {
-  const MyApp ({Key? key}) : super(key: key);
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Belajar Flutter',
       home: Scaffold(
-        backgroundColor: Colors.blueAccent,
+        backgroundColor: Color.fromARGB(255, 255, 255, 255),
         appBar: AppBar(
           title: Text('Belajar Flutter'),
-          backgroundColor: Color.fromARGB(255, 175, 171, 171),
           centerTitle: true,
+          backgroundColor: Colors.greenAccent,
         ),
-        body: BelajarContainer(),
-      )
+        body: NewsContainer(),
+      ),
     );
   }
+}
 
-  Center BelajarText() {
+class TextWidget extends StatelessWidget {
+  const TextWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
     return Center(
-        child: Text(
-          'Hello Boyy',
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 30,
-              fontWeight: FontWeight.bold,
-            ),
-        ),
-      );
+      child: Text(
+        'Hello Dunia',
+        style: TextStyle(
+            fontSize: 30,
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+            backgroundColor: Colors.black26),
+      ),
+    );
   }
 }
